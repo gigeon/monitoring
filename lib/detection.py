18a93ab8aab24ffd001c37c2f565b0ed10c4511a
gigeon <igigeon442@gmail.com>
2024-10-14 21:07:22 +0900

    객체검출 init

---
+++ lib/detection.py
@@ -0,0 +1,13 @@
+import cv2
+
+class Detection(object) :
+    def __init__(self, logger):
+        self.logger = logger
+
+    def ocr(self, img):
+        if img is None :
+            return
+    
+    def yn_detection(self, img):
+        if img is None :
+            return
