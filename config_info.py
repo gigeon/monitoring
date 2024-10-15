e15160ffb6e11ce4f25ecdf4e0a8538a7156de26
gigeon <igigeon442@gmail.com>
2024-10-15 16:08:44 +0900

    기능구현 완료

---
+++ config_info.py
@@ -0,0 +1,11 @@
+class ConfigInfo:
+    def __init__(self):
+        self.image_x = [(15, 340), (350, 680), (15, 220), (240, 450), (460, 680)]
+        self.image_y = [(150, 330), (150, 210), (420, 530), (420, 530), (420, 530)]
+        self.icon_x = [(664, 713), (724, 772), (785, 833)]
+        self.icon_y = [(8, 56), (8, 56), (8, 56)]
+        self.version_x = (35, 67)
+        self.version_y = (573, 585)
+        self.timer_x = (42, 142)
+        self.timer_y = (27, 54)
+        self.icon = ['network', 'indicator', 'qr']
