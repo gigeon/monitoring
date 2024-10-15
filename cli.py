35eb6a04bb5b6ce5f1ffde5d53e5b5d0b8d57bae
gigeon <igigeon442@gmail.com>
2024-10-15 10:25:43 +0900

    전송 주체 변경

--- cli.py
+++ cli.py
@@ -2,32 +2,39 @@ import socket
 import cv2
 import numpy
 
-#연결할 서버(수신단)의 ip주소와 port번호
+# socket 수신 버퍼를 읽어서 반환하는 함수
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf:
+            return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+# 서버에 연결할 IP와 포트
 TCP_IP = 'localhost'
 TCP_PORT = 5001
 
-#송신을 위한 socket 준비
-sock = socket.socket()
-sock.connect((TCP_IP, TCP_PORT))
-
-#OpenCV를 이용해서 webcam으로 부터 이미지 추출
-# capture = cv2.VideoCapture(0)
-img = cv2.imread('test.png', cv2.IMREAD_COLOR)
-
-
-#추출한 이미지를 String 형태로 변환(인코딩)시키는 과정
-encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
-result, imgencode = cv2.imencode('.png', img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-data = numpy.array(imgencode)
-stringData = data.tostring()
-
-#String 형태로 변환한 이미지를 socket을 통해서 전송
-sock.send( str(len(stringData)).ljust(16).encode());
-sock.send( stringData );
-sock.close()
-
-#다시 이미지로 디코딩해서 화면에 출력. 그리고 종료
-decimg=cv2.imdecode(data,1)
-cv2.imshow('CLIENT',decimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
+# TCP 소켓 준비 후 서버에 연결
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((TCP_IP, TCP_PORT))
+    
+    # 서버에서 전송한 이미지 크기 정보 수신
+    length = recvall(sock, 16)
+    if not length:
+        print("Failed to receive data length.")
+    else:
+        # 받은 길이 정보를 기반으로 이미지 데이터 수신
+        stringData = recvall(sock, int(length))
+        if stringData:
+            data = numpy.frombuffer(stringData, dtype='uint8')
+            
+            # 수신한 데이터를 이미지로 디코딩 후 출력
+            decimg = cv2.imdecode(data, 1)
+            cv2.imshow('CLIENT', decimg)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        else:
+            print("Failed to receive image data.")
--- ser.py
+++ ser.py
@@ -2,32 +2,31 @@ import socket
 import cv2
 import numpy
 
-#socket 수신 버퍼를 읽어서 반환하는 함수
-def recvall(sock, count):
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
-
-#수신에 사용될 내 ip와 내 port번호
+# 서버에서 클라이언트로 이미지를 전송하는 코드
 TCP_IP = 'localhost'
 TCP_PORT = 5001
 
-#TCP소켓 열고 수신 대기
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(True)
-conn, addr = s.accept()
+# TCP 소켓 열고 클라이언트 연결 대기
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(True)
+    print("Waiting for connection...")
+    
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected to: {addr}")
+        
+        # 전송할 이미지 로드 (웹캠 대신 이미지 파일 사용)
+        img = cv2.imread('test.png', cv2.IMREAD_COLOR)
 
-#String형의 이미지를 수신받아서 이미지로 변환 하고 화면에 출력
-length = recvall(conn,16) #길이 16의 데이터를 먼저 수신하는 것은 여기에 이미지의 길이를 먼저 받아서 이미지를 받을 때 편리하려고 하는 것이다.
-stringData = recvall(conn, int(length))
-data = numpy.fromstring(stringData, dtype='uint8')
-s.close()
-decimg=cv2.imdecode(data,1)
-cv2.imshow('SERVER',decimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
+        # 이미지를 바이너리 형태로 변환 (인코딩)
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+        result, imgencode = cv2.imencode('.png', img, encode_param)
+        data = numpy.array(imgencode)
+        stringData = data.tobytes()
+
+        # 이미지의 크기 정보를 먼저 전송
+        conn.send(str(len(stringData)).ljust(16).encode())
+        # 이미지 데이터를 전송
+        conn.send(stringData)
+        print("Image sent.")
